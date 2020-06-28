19132c2dd20916ef306863c8e8e84b545c31d821
Md Tanvir Rahaman <tanvirtex@gmail.com>
2020-06-28 23:57:16 +0600

    avoid time wasting bin/gendocs

--- config/initializers/versioning.rb
+++ config/initializers/versioning.rb
@@ -6,7 +6,8 @@
 module Peatio
   class Application
     GIT_TAG =    '2.3.5'
-    GIT_SHA =    'd3829f'
+    GIT_SHA =    'a66a960'
+    BUILD_DATE = '2020-06-28 23:53:32+06:30'
     VERSION =    GIT_TAG
   end
 end
